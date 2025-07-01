a7b3650584b7d5a18e723e3a06fd0bab311f3e02
joykahugu <joynimu1@gmail.com>
2025-07-01 10:20:01 +0300

    initial commit

---
+++ calendar.js
@@ -0,0 +1,106 @@
+//sets ensure string is not repeated e.g. name is not repeated 
+let events = [
+
+ {title:"Software class",
+    date : {
+        day: "Monday",
+        date: "2025/03/06",
+     },
+        location: "Nairobi",
+        attendees: set1= new Set(["Peter", "Jane","Joe","Mary", "George", "Adam", "Ella", "Joy"]),
+    
+
+ },
+
+
+  
+ {title:"Meeting with Sam",
+    date : {
+        day: "Tuesday",
+        date: "2025/03/07",
+    },
+        location: "Lavington",
+        attendees: set1 = new Set(["Peter", "Peter", "Jane","Joe","Mary", "George", "Adam", "Ella", "Joy"]),
+    
+   },
+ 
+ 
+
+ 
+   {title:"Software class",
+    date : {
+        day: "Wednesday",
+        date: "2025/03/08",
+     },
+        location: "Nairobi",
+        attendees: set1 = new Set(["Peter", "Jane","Joe","Mary", "George", "Adam", "Ella", "Joy"]),
+    
+
+ },
+
+
+
+  
+  {title:"Gym",
+    date : {
+        day: "Thursday",
+        date: "2025/03/09",
+    },
+        location: "Nairobi",
+        attendees: set1 = new Set(["Peter", "Jane","Joe","Mary", "George", "Adam", "Ella", "Joy"]),
+    
+
+ },
+ 
+ 
+
+  
+  {title:"",
+    date : {
+        day: "Friday",
+        date: "2025/03/10",
+     },
+        location: "Nairobi",
+        attendees: set1 = new Set(["Peter","Joe","Mary","Peter","George", "Adam", "Ella", "Joy"]),
+    
+
+ },
+ 
+ 
+
+
+
+    {title:"Picnic with James",
+    date : {
+        day: "Saturday",
+        date: "2025/03/11",
+    },
+        location: "Nairobi",
+        attendees: new Set(["Peter", "Jane","Joe","Mary", "George", "Adam", "Ella", "Joy"]),
+    
+
+ },
+
+ 
+
+
+ {title:"Soccer Match",
+    date : {
+        day: "Sunday",
+        date:"2025/03/12",
+     },
+        location: "Nairobi",
+        attendees: new Set(["Peter", "Jane","Joe","Mary", "George", "Adam", "Ella", "Joy"]),
+    
+
+ },
+]
+console.log(events)
+
+
+
+
+    
+
+
+      
